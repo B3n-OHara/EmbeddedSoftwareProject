a0212f99d3026ed4669c8d1ec60b515f460ec0c9
Ben O'Hara <128603077+B3n-OHara@users.noreply.github.com>
2023-08-10 15:55:34 +0100

    Testing of PWM functionality

---
+++ pwmTest.py
@@ -0,0 +1,9 @@
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.OUT)
+
+p = GPIO.PWM(11, 0.5)
+p.start(1)
+input('Press return to stop:')   # use raw_input for Python 2
+p.stop()
+GPIO.cleanup()
